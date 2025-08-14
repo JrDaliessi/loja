804451e8b9ecbc6a6266ef3485601e128d10500e
JrDaliessi <131720853+JrDaliessi@users.noreply.github.com>
2025-08-14 18:48:18 -0300

    refactor: update supabase client and remove auth helper

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,9 @@
 import { createBrowserClient } from '@supabase/ssr'
+import type { Database } from '@/lib/supabase.types'
 
-export function createClient() {
-  return createBrowserClient(
+export const createClient = () =>
+  createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-}
+
